3491b6d479a8167845a0eb0b0fcaf9dd814fe947
Jami Howell <jami.howell0@gmail.com>
2025-06-11 20:20:07 -0500

    Changes main to use mysql

--- main.go
+++ main.go
@@ -4,42 +4,35 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/go-sql-driver/mysql"
+	"github.com/ja-howell/stashclone/database"
 	"github.com/ja-howell/stashclone/server"
 )
-
-var db *sql.DB
 
 func main() {
 	// Capture connection properties.
 	cfg := mysql.NewConfig()
-	cfg.User = "user"
-	cfg.Passwd = "test"
+	cfg.User = os.Getenv("DBUSER")
+	cfg.Passwd = os.Getenv("DBPASS")
 	cfg.Net = "tcp"
 	cfg.Addr = "127.0.0.1:3306"
 	cfg.DBName = "stash"
 
 	// Get a database handle.
-	var err error
-	db, err = sql.Open("mysql", cfg.FormatDSN())
+	mysqldb, err := sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	pingErr := db.Ping()
+	pingErr := mysqldb.Ping()
 	if pingErr != nil {
 		log.Fatal(pingErr)
 	}
 	fmt.Println("Connected!")
-	// si := models.StashItem{
-	// 	Name: "foo",
-	// 	ID:   0,
-	// }
-	// db := database.New(map[int]models.StashItem{
-	// 	0: si,
-	// })
-	s := server.New(db)
+
+	s := server.New(database.NewMySQL(mysqldb))
 	err = s.Run()
 	if err != nil {
 		log.Fatalf("Failed to run server: %v", err)
