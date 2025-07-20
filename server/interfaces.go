ce7eeeef7bf48b86fdf6ab521e882cf1c2da2ad8
Jami Howell <jami.howell0@gmail.com>
2025-07-20 12:12:33 -0500

    Adds todo and comments

--- server/interfaces.go
+++ server/interfaces.go
@@ -3,9 +3,15 @@ package server
 import "github.com/ja-howell/stashclone/models"
 
 type Database interface {
+	// GetStashItem retrieves a single stash item by its ID.
 	GetStashItem(id int) (models.StashItem, error)
+	// ListStashItems retrieves all stash items.
+	// TODO: Add optional parameters for filtering
 	ListStashItems() ([]models.StashItem, error)
+	// DeleteStashItem deletes a stash item by its ID.
 	DeleteStashItem(id int) error
+	// CreateStashItem creates a new stash item.
 	CreateStashItem(si models.StashItem) error
+	// UpdateStashItem updates an existing stash item by its ID.
 	UpdateStashItem(id int, si models.StashItem) error
 }
